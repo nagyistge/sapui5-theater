ef7b5259df52fb91c57a7f0e03a58c6c54b09568
Arn-O <rnd.bertrand@gmail.com>
2014-12-16 13:13:28 +0100

    stub for web project and the ux3 shell

---
+++ sapui5-theater-web/WebContent/test-ux3-shell/home.view.js
@@ -0,0 +1,19 @@
+sap.ui.jsview("test-ux3-shell.home", {
+
+	/** Specifies the Controller belonging to this View. 
+	* In the case that it is not implemented, or that "null" is returned, this View does not have a Controller.
+	* @memberOf test-ux3-shell.home
+	*/ 
+	getControllerName : function() {
+		return "test-ux3-shell.home";
+	},
+
+	/** Is initially called once after the Controller has been instantiated. It is the place where the UI is constructed. 
+	* Since the Controller is given to this method, its event handlers can be attached right away. 
+	* @memberOf test-ux3-shell.home
+	*/ 
+	createContent : function(oController) {
+
+	}
+
+});
