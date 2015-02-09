2e70e7d965f108eb2e293886ef3dec4ea94f483d
Arn-O <rnd.bertrand@gmail.com>
2015-02-09 22:47:53 +0100

    nightly build

--- tools/PyMusicDBLoader/PyMusicDBLoader.py
+++ tools/PyMusicDBLoader/PyMusicDBLoader.py
@@ -1,13 +1,26 @@
 #!/usr/bin/env python
 
-import requests
-import json
 import xml.etree.ElementTree as ET
 
+import requests
+import argparse
+import json
+
+def get_param():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-s", "--svc", help="svc URL")
+    parser.add_argument("-f", "--filedb", help="music DB file")
+    args = parser.parse_args()
+    return args.svc, args.filedb
+
+
 if __name__=='__main__':
-    odata_svc = 'https://sapui5theateri305327trial.hanatrial.ondemand.com/sapui5-theater-web/theater.svc/'
+    svc_url, fmusicdb = get_param()
+
     s = requests.Session()
-    artist_url = odata_svc + "/Artists"
+
+    artist_url = svc_url + "/Artists"
+    album_url = svc_url + "/Albums"
 
     headers = {
         'OData-Version': '4.0',
@@ -16,20 +29,56 @@ if __name__=='__main__':
         }
 
     payload = {
-        #"Id": "1",
         "MusicBrainzArtistID": "Mb7539c32-53e7-4908-bda3-81449c367da6",
         "Name": "Lana Del Rey",
         }
 
     r = s.post(artist_url, data=json.dumps(payload), headers=headers)
-    print r.status_code, r.text
+    print "Artist: %s" % r.status_code
 
-    tree = ET.parse('musicdb.xml')
-    root = tree.getroot()
+    payload = {
+        "Compilation": "False",
+        "Label": "Unknown",
+        "MusicBrainzAlbumID": "sdcds",
+        "Rating": "5",
+        "Review": "Nice album",
+        "Title": "Born to die",
+        "YearRelease": "2012",
+        }
+
+    r = s.post(album_url, data=json.dumps(payload), headers=headers)
+    print "Album1: %s" % r.status_code
+
+    payload = {
+        "Compilation": "False",
+        "Label": "Unknown",
+        "MusicBrainzAlbumID": "sdcds",
+        "Rating": "5",
+        "Review": "Another nice album",
+        "Title": "Ultraviolence",
+        "YearRelease": "2014",
+        }
+
+    r = s.post(album_url, data=json.dumps(payload), headers=headers)
+    print "Album2: %s" % r.status_code
+
+    ref_url = album_url + "/Albums(4L)/Artist/$ref"
+
+    payload = {
+        "@odata.id": "Artist(1L)"
+        }
+
+    r = s.post(ref_url, data=json.dumps(payload), headers=headers)
+    print "Ref: %s" % r.status_code
+    print r.text
+
+
+    #tree = ET.parse('musicdb.xml')
+    #root = tree.getroot()
     #print "Albums:"
     #for album in root.findall('album'):
     #    print '\t' + album.find('title').text
     #print
-    print "Artists:"
-    for artist in root.findall('artist'):
-        print '\t' + artist.find('name').text + ' (' + artist.find('musicBrainzArtistID').text + ')'
+    #print "Artists:"
+    #for artist in root.findall('artist'):
+    #    print '\t' + artist.find('name').text + ' (' + artist.find('musicBrainzArtistID').text + ')'
