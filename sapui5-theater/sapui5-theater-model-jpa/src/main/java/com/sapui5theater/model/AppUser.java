b67da0bd3e6b92abce5fbd59a65d8baaf16df78b
Arn-O <rnd.bertrand@gmail.com>
2015-03-13 13:19:49 +0100

    application user data entity

---
+++ sapui5-theater/sapui5-theater-model-jpa/src/main/java/com/sapui5theater/model/AppUser.java
@@ -0,0 +1,63 @@
+package com.sapui5theater.model;
+
+import java.util.List;
+
+import javax.persistence.*;
+
+@Entity
+@NamedQuery(name = "AllAppUsers", query = "select au from AppUser au")
+public class AppUser {
+
+	public AppUser() {
+	}
+
+	@Id
+	@Column(name = "USER_ID")
+	private String userId;
+	
+	private String kodiIP;
+	private int kodiPort;
+	private String kodiUser;
+	private String kodiPassword;
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getKodiIP() {
+		return kodiIP;
+	}
+
+	public void setKodiIP(String kodiIP) {
+		this.kodiIP = kodiIP;
+	}
+	
+	public int getKodiPort() {
+		return kodiPort;
+	}
+
+	public void setKodiPort(int kodiPort) {
+		this.kodiPort = kodiPort;
+	}
+	
+	public String getKodiUser() {
+		return kodiUser;
+	}
+
+	public void setKodiUser(String kodiUser) {
+		this.kodiUser = kodiUser;
+	}
+	
+	public String getKodiPassword() {
+		return kodiPassword;
+	}
+
+	public void setKodiPassword(String kodiPassword) {
+		this.kodiPassword = kodiPassword;
+	}
+	
+}
